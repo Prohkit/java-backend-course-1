ef5f2c8130906f425bebd01922b827427bb47992
Никита <nikproserg@gmail.com>
2023-11-24 02:47:00 +0800

    task 2 test another try

--- src/test/java/edu/hw6/task2/Task2Test.java
+++ src/test/java/edu/hw6/task2/Task2Test.java
@@ -2,6 +2,7 @@ package edu.hw6.task2;
 
 import edu.hw6.Task2;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,14 +16,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task2Test {
     private static final Path FILE =
-        Path.of("./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret.txt").toAbsolutePath();
+        Path.of("./src/test/java/edu/hw6/task2/files/Tinkoff Bank Biggest Secret.txt").toAbsolutePath();
     private static final Path COPY =
-        Path.of("./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret - copy.txt").toAbsolutePath();
+        Path.of("./src/test/java/edu/hw6/task2/files/Tinkoff Bank Biggest Secret - copy.txt").toAbsolutePath();
     private static final Path COPY_SECOND = Path.of(
-        "./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret - copy (2).txt").toAbsolutePath();
+        "./src/test/java/edu/hw6/task2/files/Tinkoff Bank Biggest Secret - copy (2).txt").toAbsolutePath();
 
     @BeforeAll
-    static void init() {
+    public static void init() {
         Task2 task2 = new Task2();
         task2.cloneFile(FILE);
         task2.cloneFile(FILE);
@@ -30,18 +31,20 @@ public class Task2Test {
 
     @Test
     void task2() {
-        int expectedCount = 4;
-        try (Stream<Path> files = Files.list(Paths.get("./src/test/java/edu/hw6/task2").toAbsolutePath())) {
+        int expectedCount = 3;
+        try (Stream<Path> files = Files.list(Paths.get("./src/test/java/edu/hw6/task2/files").toAbsolutePath())) {
             long count = files.count();
             assertThat(count)
                 .isEqualTo(expectedCount);
+
+            destroy();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @AfterAll
-    static void destroy() throws IOException {
+    public static void destroy() throws IOException {
         Files.deleteIfExists(COPY);
         Files.deleteIfExists(COPY_SECOND);
     }
