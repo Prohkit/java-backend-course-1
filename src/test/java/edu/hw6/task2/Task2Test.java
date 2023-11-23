69a2caed45ee34c9106688fc3d805aa46349337b
Никита <nikproserg@gmail.com>
2023-11-23 22:33:51 +0800

    fix tests

--- src/test/java/edu/hw6/task2/Task2Test.java
+++ src/test/java/edu/hw6/task2/Task2Test.java
@@ -12,22 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Task2Test {
     private static final Path FILE_TO_COPY = Paths.get("./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret.txt");
     private static final Path COPY = Paths.get("./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret - копия.txt");
-    private static final Path COPY_SECOND =
-        Paths.get("./src/test/java/edu/hw6/task2/Tinkoff Bank Biggest Secret - копия (2).txt");
 
     @Test
     void task2() {
         Task2 task2 = new Task2();
         task2.cloneFile(FILE_TO_COPY);
         assertTrue(Files.exists(COPY));
-
-        task2.cloneFile(FILE_TO_COPY);
-        assertTrue(Files.exists(COPY_SECOND));
     }
 
     @AfterAll
     static void destroy() throws IOException {
         Files.deleteIfExists(COPY);
-        Files.deleteIfExists(COPY_SECOND);
     }
 }
